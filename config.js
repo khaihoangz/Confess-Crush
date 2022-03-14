d45616017eeadd7ac3fa0566859e4ff26fbe48a3
khaikim0705 <101533445+khaikim0705@users.noreply.github.com>
2022-03-14 08:08:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
 const CONFIG = {
     titleWeb: "Nhớ bé",
     introTitle: 'Vũ Thị kim Ngân ',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Anh cá với em rằng em là tay trộm chuyên nghiệp.
+    Bởi vì anh mới nhìn thấy em ở đây và trong nháy mắt là em đã đánh cắp trái tim của anh rồi`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
@@ -13,8 +11,8 @@ const CONFIG = {
     question: 'Trên thế giới hơn 7 tỉ người mà sao Bé lại yêu ah <3',
     btnReply: 'Gửi cho Bé <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    mess: 'Mình biết mà 🥰. Yêu Bé nhiều nhiều 😘😘',
     messDesc: 'Trưa nay 12h, ah qua nhà bé chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Dạ~~~<3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
