c59a7e192f342ba80f67315086c3a0150fb8f665
khaihoangz <111667852+khaihoangz@users.noreply.github.com>
2022-08-21 08:08:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     titleWeb: "nói nghe nè",
     introTitle: 'Đố vui',
-    introDesc: `trâm cute hột me xí nguội :>`,
+    introDesc: `Hoàng đẹp zai :>`,
     btnIntro: '^^HEHE^^',
     title: 'đố m  t đng nghĩ m mập hay gầy',
     desc: 'mời trả lời :33 ',
     btnYes: 'gầy đó :33',
     btnNo: 'mập nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao m lại nhịn ăn thế kia <3',
-    btnReply: 'Gửi cho trâm ',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao Lại không ai yêu thế này <3',
+    btnReply: 'Gửi cho bản thân 1 năm sau :))  ',
     reply: 'nhớ ăn uống đầy đủ nhá :3',
     mess: 'ăn nhiều vào đó',
-    messDesc: 'Người khỏe t mới chở m đi chs dc .',
-    btnAccept: 'ăn đi nhá',
+    messDesc: 'khỏe mới đi chs được .',
+    btnAccept: 'Chúc may mắn ^^',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
